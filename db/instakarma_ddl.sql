2b53d20057f3093e2ef691479d024e8d7ed2c9ad
Chris Cowell <christopher.w.cowell@gmail.com>
2024-10-20 20:13:54 -0700

    WIP

--- db/instakarma_ddl.sql
+++ db/instakarma_ddl.sql
@@ -3,26 +3,28 @@
 PRAGMA foreign_keys = ON; -- Weirdly required by SQLite
 
 -- optimizations for expected workload: DB spread across multiple files
-PRAGMA journal_mode = WAL;
-PRAGMA synchronous = NORMAL;
+-- PRAGMA journal_mode = WAL;
+-- PRAGMA synchronous = NORMAL;
 
 -- store current (not historical) karma for all entities
-CREATE TABLE entities (
-    entity_id   INTEGER  PRIMARY KEY, -- SQLite autoincrements primary keys automatically
-    entity_name TEXT     NOT NULL UNIQUE, -- Slack username like `@bob`, or name of non-user entity like `Banyan`
-    user_id     TEXT     UNIQUE, -- Slack user ID; not used for entities like 'Banyan'
-    karma       INTEGER  NOT NULL DEFAULT 0, -- could be calculated from grants table, but is here for efficiency
-    disabled    BOOLEAN  NOT NULL DEFAULT FALSE, -- 'true' means entity doesn't want to participate in instakarma
-    created_at  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP -- no plans for use, but might be useful in future?
+CREATE TABLE entities
+(
+    entity_id  INTEGER PRIMARY KEY,                        -- SQLite autoincrements primary keys automatically
+    name       TEXT     NOT NULL UNIQUE,                   -- Slack username like '@bob', or name of non-user entity like 'banyan'
+    user_id    TEXT UNIQUE,                                -- Slack user ID; not used for entities like 'Banyan'
+    karma      INTEGER  NOT NULL DEFAULT 0,                -- could be calculated from grants table, but is here for efficiency
+    disabled   BOOLEAN  NOT NULL DEFAULT FALSE,            -- 'true' means entity doesn't want to participate in instakarma
+    created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP -- no plans for use, but might be useful in future?
 );
 
 -- log individual karma grants for auditing
-CREATE TABLE grants (
-    grant_id      INTEGER  PRIMARY KEY, -- SQLite autoincrements primary keys automatically
-    granter_id    INTEGER  NOT NULL,
-    recipient_id  INTEGER  NOT NULL,
-    amount        INTEGER  NOT NULL CHECK (amount IN (-1, 1)),
-    timestamp     DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+CREATE TABLE grants
+(
+    grant_id     INTEGER PRIMARY KEY, -- SQLite autoincrements primary keys automatically
+    granter_id   INTEGER  NOT NULL,
+    recipient_id INTEGER  NOT NULL,
+    amount       INTEGER  NOT NULL CHECK (amount IN (-1, 1)),
+    timestamp    DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     FOREIGN KEY (granter_id) REFERENCES entities (entity_id),
     FOREIGN KEY (recipient_id) REFERENCES entities (entity_id)
 );
@@ -30,4 +32,4 @@ CREATE TABLE grants (
 -- speed up lookups of what entity_name a user_id corresponds to
 -- CREATE INDEX idx_user_id ON entities(user_id); -- compare performance with and without this, or just delete it
 
--- automatically creates implicit index on `entity_name` in `entities` table due to `UNIQUE` keyword
+-- 'UNIQUE' keyword automatically creates index on 'entities.name'
