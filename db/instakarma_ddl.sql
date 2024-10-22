789264d1b721689ebd4977f3aebc6a9866446a37
Chris Cowell <43705622+cwcowell@users.noreply.github.com>
2024-10-22 12:12:41 -0700

    streamline DDL

--- db/instakarma_ddl.sql
+++ db/instakarma_ddl.sql
@@ -1,17 +1,13 @@
 -- data definitions for SQLite 3.45
 
-PRAGMA foreign_keys = ON; -- Weirdly required by SQLite
-
--- optimizations for the expected workload -- this might spread the DB spread across multiple files
--- PRAGMA journal_mode = WAL;
--- PRAGMA synchronous = NORMAL;
+PRAGMA foreign_keys = ON; -- weirdly required by SQLite
 
 -- store current (not historical) karma for all entities
 CREATE TABLE entities
 (
     entity_id  INTEGER PRIMARY KEY,                        -- SQLite autoincrements primary keys automatically
     name       TEXT     NOT NULL UNIQUE,                   -- Slack username like '@bob', or name of non-user entity like 'banyan'
-    user_id    TEXT UNIQUE,                                -- Slack user ID; not used for entities like 'Banyan'
+    user_id    TEXT UNIQUE,                                -- Slack user ID; not used for non-user entities like 'banyan'
     karma      INTEGER  NOT NULL DEFAULT 0,                -- could be calculated from grants table, but is here for efficiency
     disabled   BOOLEAN  NOT NULL DEFAULT FALSE,            -- 'true' means entity doesn't want to participate in instakarma
     created_at DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP -- no plans for use, but might be useful in future?
@@ -29,7 +25,4 @@ CREATE TABLE grants
     FOREIGN KEY (recipient_id) REFERENCES entities (entity_id)
 );
 
--- speed up lookups of what entity_name a user_id corresponds to
--- CREATE INDEX idx_user_id ON entities(user_id); -- compare performance with and without this, or just delete it
-
--- 'UNIQUE' keyword automatically creates index on 'entities.name'
+-- 'UNIQUE' keyword automatically creates indices on 'entities.name' and 'entities.user_id'
