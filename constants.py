a1c41326f07fcdae5839de2254e1ba02e55568b3
Chris Cowell <christopher.w.cowell@gmail.com>
2024-10-24 13:52:54 -0700

    Add /instakarma stats

--- constants.py
+++ constants.py
@@ -9,3 +9,7 @@ LOG_FILE_COUNT: Final[int] = 3
 
 LOG_LEVEL: Final[str] = 'DEBUG'
 # LOG_LEVEL: Final[str] = 'INFO'
+
+# for use with `/instakarma stats`
+NUM_TOP_GRANTERS: Final[int] = 5
+NUM_TOP_RECIPIENTS: Final[int] = 5
