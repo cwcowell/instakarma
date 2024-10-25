447582344a9b0c60c37f727378815c19c761e832
Chris Cowell <christopher.w.cowell@gmail.com>
2024-10-25 09:53:51 -0700

    Change `log/` to `logs/`

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ from typing import Final
 DB_FILE: Final[str] = 'db/instakarma.db'
 DB_DDL_FILE: Final[str] = 'db/instakarma_ddl.sql'
 
-LOG_FILE: Final[str] = 'log/instakarma.log'
+LOG_FILE: Final[str] = 'logs/instakarma.log'
 LOG_FILE_SIZE: Final[int] = 1024 * 1024 * 5
 LOG_FILE_COUNT: Final[int] = 3
 
