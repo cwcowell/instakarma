4b1a5508884c86567864c3b90949a3f9256efac6
Chris Cowell <christopher.w.cowell@gmail.com>
2024-10-28 12:45:26 -0700

    wip - admin app

--- constants.py
+++ constants.py
@@ -3,6 +3,9 @@ from typing import Final
 DB_FILE: Final[str] = 'db/instakarma.db'
 DB_DDL_FILE: Final[str] = 'db/instakarma_ddl.sql'
 
+# for use with instakarma-admin
+GRANTS_EXPORT_FILE: Final[str] = 'grants.csv'
+
 LOG_FILE: Final[str] = 'logs/instakarma.log'
 LOG_FILE_SIZE: Final[int] = 1024 * 1024 * 5
 LOG_FILE_COUNT: Final[int] = 3
@@ -13,3 +16,4 @@ LOG_LEVEL: Final[str] = 'DEBUG'
 # for use with `/instakarma stats`
 NUM_TOP_GRANTERS: Final[int] = 5
 NUM_TOP_RECIPIENTS: Final[int] = 5
+
