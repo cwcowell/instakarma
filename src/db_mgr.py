2998a3809dd5e40d0770c4a15027fa72966710c6
Chris Cowell <christopher.w.cowell@gmail.com>
2024-12-03 08:39:19 -0800

    wip

--- src/db_mgr.py
+++ src/db_mgr.py
@@ -6,41 +6,47 @@ from pathlib import Path
 from sqlite3 import Connection, Cursor
 import sqlite3
 import sys
-from typing import Self, ClassVar
 
 
 class DbMgr:
-    """Collect all DB-related methods in one singleton class."""
+    """Collect all DB-related methods in one class."""
 
-    @classmethod
-    def execute_statement(cls, statement: str, params: tuple = ()) -> list[tuple]:
-        """Execute SQL statement and return results.
+    def __init__(self, logger: Logger):
+        self.logger: Logger = logger
+
+    def get_db_connection(self) -> Connection:
+        """Open and return a DB connection.
+
+        :returns: Connection object to DB
+        :raises sqlite3.Error: If it can't connect to the DB
+        """
+        try:
+            return sqlite3.connect(DB_FILE_NAME)
+        except sqlite3.Error as e:
+            self.logger.critical(StringMgr.get_string('db.error.connection', db_file_name=DB_FILE_NAME, e=e))
+            raise
+
+    def execute_statement(self, statement: str, params: tuple = ()) -> list[tuple]:
+        """Open a DB connection, execute an SQL statement, close the connection.
 
         :returns: List of results as tuples
         :raises sqlite3.Error: If something goes wrong with the DB
         """
-        instance = cls()
 
-        try:
-            cursor: Cursor = instance._connection.execute(statement, params)
-            instance._connection.commit()  # no-op for SELECT statements
-            return cursor.fetchall()
+        log_friendly_statement: str = self.format_statement_for_log(statement)
+        with self.get_db_connection() as conn:
+            try:
+                cursor: Cursor = conn.execute(statement, params)
+                conn.commit()
+                return cursor.fetchall()
+            except sqlite3.Error as e:
+                conn.rollback()
+                self.logger.error(StringMgr.get_string('db.error.rollback',
+                                                       statement=log_friendly_statement,
+                                                       parms=params, e=e))
+                raise
 
-        except sqlite3.Error as e:
-            instance._connection.rollback()
-            log_friendly_statement: str = instance.format_statement_for_log(statement)
-            instance.logger.error(StringMgr.get_string('db.error.rollback',
-                                                   statement=log_friendly_statement,
-                                                   parms=params,
-                                                   e=e))
-            raise
-
-    def __del__(self):
-        """Close up the database connection"""
-        self._connection.close()
-
-    @classmethod
-    def init_db(cls) -> str:
+    def init_db(self) -> str:
         """Create an empty DB if it doesn't already exist.
 
         No-op if there is already a DB.
@@ -54,25 +60,25 @@ class DbMgr:
 
         if db_path.exists():
             msg: str = StringMgr.get_string('db.exists', db_path=db_path.resolve())
-            cls.logger.info(msg)
+            self.logger.info(msg)
             return msg
-        with open(db_ddl_path) as ddl_file:
-            ddl: str = ddl_file.read()
-        try:
-            cls.conn.executescript(ddl)
-            cls.conn.commit()
-        except sqlite3.Error as e:
-            msg: str = StringMgr.get_string('db.error.could-not-create', e=e)
-            cls.logger.critical(msg)
+        with self.get_db_connection() as conn:
+            with open(db_ddl_path) as ddl_file:
+                ddl: str = ddl_file.read()
+            try:
+                conn.executescript(ddl)
+                conn.commit()
+            except sqlite3.Error as e:
+                msg: str = StringMgr.get_string('db.error.could-not-create', e=e)
+                self.logger.critical(msg)
+                return msg
+            msg: str = StringMgr.get_string('db.created-new',
+                                            db_path=db_path.resolve(),
+                                            db_ddl_path=db_ddl_path.resolve())
+            self.logger.info(msg)
             return msg
-        msg: str = StringMgr.get_string('db.created-new',
-                                        db_path=db_path.resolve(),
-                                        db_ddl_path=db_ddl_path.resolve())
-        cls.logger.info(msg)
-        return msg
 
-    @staticmethod
-    def format_statement_for_log(statement: str) -> str:
+    def format_statement_for_log(self, statement: str) -> str:
         """Format a statement as a single line for logging.
 
         :returns: Statement formatted as a single line
@@ -84,8 +90,7 @@ class DbMgr:
         statement = statement.replace('\n', ' ')  # replace newlines with spaces
         return ' '.join(statement.split())  # replace multiple spaces with a single space
 
-    @classmethod
-    def backup_db(cls) -> None:
+    def backup_db(self) -> None:
         """Copy the DB file to another local file.
 
         This should only be called from `instakarma-admin`, so it exits on failure instead of logging and raising.
@@ -101,18 +106,19 @@ class DbMgr:
             sys.exit(StringMgr.get_string('db.error.db-backup-file-exists', db_backup_path=db_backup_path))
 
         # checkpoint to truncate WAL and consolidate DB to 1 file
-        try:
-            cursor: Cursor = self.conn.execute('PRAGMA wal_checkpoint(TRUNCATE);')
-            results: tuple[int, int, int] = cursor.fetchone()
-            if results[0]:
-                sys.exit(StringMgr.get_string('db.error.truncation-blocked'))
-            print(StringMgr.get_string('db.truncated', single_db_file=db_path.resolve()))
-        except sqlite3.Error as e:
-            sys.exit(StringMgr.get_string('db.error.could-not-truncate', e=e))
+        with self.get_db_connection() as conn:
+            try:
+                cursor: Cursor = conn.execute('PRAGMA wal_checkpoint(TRUNCATE);')
+                results: tuple[int, int, int] = cursor.fetchone()
+                if results[0]:
+                    sys.exit(StringMgr.get_string('db.error.truncation-blocked'))
+                print(StringMgr.get_string('db.truncated', single_db_file=db_path.resolve()))
+            except sqlite3.Error as e:
+                sys.exit(StringMgr.get_string('db.error.could-not-truncate', e=e))
 
         try:
             with sqlite3.connect(DB_FILE_NAME) as source, \
-                    sqlite3.connect(DB_BACKUP_FILE_NAME) as destination:
+                 sqlite3.connect(DB_BACKUP_FILE_NAME) as destination:
                 source.backup(destination)
         except sqlite3.Error as e:
             sys.exit(StringMgr.get_string('db.error.could-not-backup', e=e))
--- src/entity_dao.py
+++ src/entity_dao.py
@@ -1,12 +1,33 @@
 from entity import Entity
+from enums import Status
 from db_mgr import DbMgr
+from string_mgr import StringMgr
+
+from logging import Logger
+import sqlite3
 
 
 class EntityDAO:
 
-    db_mgr: DbMgr = DbMgr()
+    def __init__(self, db_mgr: DbMgr, logger: Logger):
+        self.db_mgr = db_mgr
+        self.logger = logger
 
-    def list_opted_out(cls) -> list[Entity]:
+    def is_in_db(self, name: str) -> bool:
+        sql: str = """
+                   SELECT name
+                   FROM entities
+                   WHERE name = ?;        
+                   """
+        params: tuple = (name,)
+        try:
+            results: list[tuple] = self.db_mgr.execute_statement(sql, params)
+        except sqlite3.Error as e:
+            self.logger.error(StringMgr.get_string('entity.error.could-not-check-name', name=name, e=e))
+            raise
+        return len(results) > 0
+
+    def list_opted_out(self) -> list[Entity]:
         sql: str = """
             SELECT name
             FROM entities
@@ -20,3 +41,22 @@ class EntityDAO:
             entity: Entity = Entity(name=name)
             entities.append(entity)
         return entities
+
+    def set_status(self, name: str, new_status: Status) -> None:
+        sql: str = f"""
+                   UPDATE entities
+                   SET opted_in = ?
+                   WHERE name = ?;
+                   """
+        params: tuple = ((new_status == Status.OPTED_IN), name)
+        try:
+            self.db_mgr.execute_statement(sql, params)
+            self.logger.info(StringMgr.get_string(key_path='entity.current-status',
+                                                  name=name,
+                                                  status=new_status.value))
+        except sqlite3.Error as e:
+            self.logger.error(StringMgr.get_string(key_path='entity.error.could-not-set-status',
+                                                   name=name,
+                                                   status=new_status.value,
+                                                   e=e))
+            raise
